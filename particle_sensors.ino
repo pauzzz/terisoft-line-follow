eae6e1f78d772b1d4c6ff11a65e74743ad2806b1
Paul Wong <paulwong001@gmail.com>
2018-04-23 19:29:42 -0400

    cleaned up code, don't know if it works.

--- particle_sensors.ino
+++ particle_sensors.ino
@@ -1,22 +1,25 @@
-#include <QTRSensors.h> //not inherently in particle libraries... idk if that'll be an issue.
-#include <Phobot.h>// for motor
+#include <QTRSensors.h> 	//not inherently in particle libraries... idk if that'll be an issue. upload to src/
+#include <PhoBot.h>			// for motor
+#include <string>
 
-Phobot go=Phobot(9.0, 6.0);
+PhoBot go=PhoBot(9.0, 6.0);
 
 int KD = 0; 	//experiment from 0-> 1 for good value
 int KP = 0; 	//experiment from 1-> 10 for good value
-int M1_DEFAULT_SPEED=70;
-int M2_DEFAULT_SPEED=70;
+int M1_DEFAULT_SPEED=130;
+int M2_DEFAULT_SPEED=130;
 int rightMaxSpeed=200;
 int leftMaxSpeed=200;
 int lastError;
 
 int calibrationCheck=0;
+int DEBUG = digitalRead(D3);//D3 random port for now, need to write debugging code
 
-int num_sensors=8; 		//number of sensors we're using - CHANGE if not using all 8
+
+const int num_sensors=8; 		//number of sensors we're using - CHANGE if not using all 8
 int emit_pin=D2; 		//set emit pin to D0 for now - can change to open pin later
 int Timeout=10000; 		//10 second time out for setting up the sensors
-int calibrationCheck=0; //initialize calibration number
+
 QTRSensorsRC qtrrc((unsigned char[]) {8,7,6,5,4,3,2,1}, num_sensors, Timeout, emit_pin);    // define pin# later when I know which ones are free - prolly wont need all 8
 unsigned int sensorValues[num_sensors]; 													//array of the number of sensors
 
@@ -52,44 +55,49 @@ int error = position - 3500; 							//3500 is for 8 sensors
 int motorSpeed = KD * error + KP * (error - lastError);
 lastError = error;
 
-int leftMotorSpeed = ((M1_DEFAULT_SPEED + motorSpeed)/(leftMaxSpeed))*100;	//the math should give us scaling to use for PhoBot -> PhoBot uses 0 to 100 duty
-int rightMotorSpeed = ((M2_DEFAULT_SPEED - motorSpeed)/(rightMaxSpeed))*100; 	//these will turn the bot - change signs if wrong direction
 
-MotorSpeed(leftMotorSpeed, rightMotorSpeed) 							// !!! PhoBot setMotors not used in this way - MUST change
+	motorSpeedLimit(motorSpeed);
     
 //CALIBRATION
 do {
 calibration();
 }
 
-while (calibrationCheck==1)//
+while (calibrationCheck==1);//
 
 if(DEBUG){
 calibrationCheck++; // run calibration when debug
 }
+}
 
-
-void calibration(){
+void calibration() {
 for (int i=0; i<400;i++) //400 made up num, can increase to extend calibration time.
 {
     qtrrc.calibrate(); // reads sensors idk speed or frequency here - not arduino
 	delay(20); //added delay to ensure calibration finishes
 }
 
-digitalWrite(D0, LOW)//CHANGE D0 TO APPROPRIATE PIN LATER - runs once calibrations is finished - connect to an LED to say its finished.
+digitalWrite(D0, LOW);//CHANGE D0 TO APPROPRIATE PIN LATER - runs once calibrations is finished - connect to an LED to say its finished.
 calibrationCheck=0;
 lastError=0;
 }
-}
 
-void MotorSpeed(int leftMSpd, int rightMSpd){
+
+void motorSpeedLimit(int MSpd){
 	
 //speed limiting
-if (leftMSpd > 100 ) leftMSpd = 100; 		// prevent the motor from going beyond max speed
-if (rightMSpd > 100 ) rightMSpd = 100;		// prevent the motor from going beyond max speed
-if (leftMSpd < 0) leftMSpd = 0; 							// keep the motor speed positive
-if (leftMSpd < 0) rightMSpd = 0; 							// keep the motor speed positive
 
-go.setMotors("M1-F-leftMSpd") 				//must convert MSpd to string and append to this... idk how right now
-go.setMotors("M2-F-rightMSpd")
+int leftMSpd = ((M1_DEFAULT_SPEED + MSpd)/(leftMaxSpeed))*100;	//the math should give us scaling to use for PhoBot -> PhoBot uses 0 to 100 duty
+int rightMSpd = ((M2_DEFAULT_SPEED - MSpd)/(rightMaxSpeed))*100; 	//these will turn the bot - change signs if wrong direction
+
+			
+if (leftMSpd > 100 ) leftMSpd = 100; 			// prevent the motor from going beyond max speed
+if (rightMSpd > 100 ) rightMSpd = 100;		// prevent the motor from going beyond max speed
+if (leftMSpd < 0) leftMSpd = 0; 				// keep the motor speed positive
+if (rightMSpd < 0) rightMSpd = 0; 			// keep the motor speed positive
+
+
+
+go.setMotors("M1-F-"+String(leftMSpd)); 						//will this work? 
+go.setMotors("M2-F-"+String(rightMSpd));
 }
